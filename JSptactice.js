ff7d2ed9f6b6008ec4ea6d8f27d55da84a2bcd28
Amusenna <ahmet@visiosoft.com.tr>
2022-01-09 16:22:23 +0100

    first commit

---
+++ JSptactice.js
@@ -0,0 +1,27 @@
+var name = prompt("What is your name ? ");
+
+var firstChar = name.slice(0,1);
+
+var upperCasefirstChar = firstChar.toUpperCase();
+
+var restOfname = name.slice(1,name.length);
+
+restOfname = restOfname.toLowerCase();
+
+var capitalisedName= upperCasefirstChar + restOfname ;
+
+alert("Hello; " + capitalisedName);
+-----------------------------------------------------------
+
+var a = 16 % 4;
+0
+
+var x = 3 ;
+
+var y = x++;
+
+y += 1 ;
+
+y ? y = 4
+
+-------------------------------------------------------------
